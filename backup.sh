6396fd1af58f586ff3294fec94dd74fa3f7a7d28
testforworkn8n <testforworkn8n@gmail.com>
2025-10-06 14:58:27 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-
 echo "Starting backup loop..."
 
 while true
 do
     echo "Running backup..."
-    # Sync /data to GitHub repo folder
     cp -r /data /backup
-
-    cd /backup || exit
+    cd /backup || exit 1
     git add .
-    git commit -m "Automated n8n backup $(date)"
+    git commit -m "Automated backup $(date)"
     git push https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git main
-
-    echo "Backup complete. Sleeping 24h..."
+    echo "Sleeping 24h..."
     sleep 86400
 done
